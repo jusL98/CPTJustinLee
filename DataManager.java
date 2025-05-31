0e4bb80f2beeb032c0ed42f8f6ecd3dd26b90f76
Justin Matthew Lee <justin.matthew.lee.18@gmail.com>
2025-05-30 20:24:22 -0400

    Created leaderboard method with bubble sort

--- DataManager.java
+++ DataManager.java
@@ -11,6 +11,7 @@ public class DataManager {
 		String strBoardTitle = "";
 		boolean boolKeepSearching = true;
 		
+		// Locate theme that matches strSelectedTheme parameter and fill all values
 		while(themes.eof() != true && boolKeepSearching == true){
 			strThemeName = themes.readLine();
 			if(strThemeName.equalsIgnoreCase(strSelectedTheme)){
@@ -22,14 +23,66 @@ public class DataManager {
 			strBoardTitle = themes.readLine();
 		}
 		
-		
-			
-		System.out.println(strThemeName + strP1Color + strP2Color + strBoardColor + strBoardTitle);
+		System.out.println("TEST: Theme Properties -> " + strThemeName + "  " + strP1Color + "  " +  strP2Color + "  " +  strBoardColor + "  " + strBoardTitle); // TEST
 		
 		themes.close();
 	}
 	
-	public static void main(String[] args){
+	public static String[][] viewLeaderboard() {
+		// Count number of entries in leaderboard data file
+		TextInputFile leaderboard = new TextInputFile("leaderboard.txt");
+		String strName;
+		int intWins;
+		int intNumEntries = 0;
+		while(leaderboard.eof() != true){
+			strName = leaderboard.readLine();
+			intWins = leaderboard.readInt();
+			
+			intNumEntries++;
+		}
+		leaderboard.close();
+		
+		System.out.println("TEST: Num LB Entries -> " + intNumEntries); // TEST
+		
+		// Create and fill leaderboard array with values from data file
+		leaderboard = new TextInputFile("leaderboard.txt");
+		String strEntries[][] = new String[intNumEntries][2];
+		int intCount;
+		for(intCount = 0; intCount < intNumEntries; intCount++){
+			strEntries[intCount][0] = leaderboard.readLine();
+			strEntries[intCount][1] = leaderboard.readLine();
+		}
+		leaderboard.close();
+		
+		// Sort leaderboard array in descending order (highest to least wins)
+		int intCount2;
+		String strNameTemp;
+		String strWinsTemp;
+		for(intCount = 0; intCount < strEntries.length - 1; intCount++){
+			for(intCount2 = 0; intCount2 < strEntries.length - 1; intCount2++){
+				if(Integer.parseInt(strEntries[intCount2][1]) < Integer.parseInt(strEntries[intCount2+1][1])){
+					strNameTemp = strEntries[intCount2][0];
+					strEntries[intCount2][0] = strEntries[intCount2+1][0];
+					strEntries[intCount2+1][0] = strNameTemp;
+					
+					strWinsTemp = strEntries[intCount2][1];
+					strEntries[intCount2][1] = strEntries[intCount2+1][1];
+					strEntries[intCount2+1][1] = strWinsTemp;
+				}
+			}
+		}
+		
+		// TBD - only return top 10 scores
+		
+		return strEntries;
+	}
+	
+	public static void main(String[] args) {
 		loadTheme("christmas");
+		String strLBEntries[][] =  viewLeaderboard();
+		
+		for(int intCount = 0; intCount < strLBEntries.length; intCount++){
+			System.out.println(strLBEntries[intCount][0] + ", " + strLBEntries[intCount][1]);
+		}
 	}
 }
